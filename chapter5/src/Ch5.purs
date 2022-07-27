2cbb385ed062035bd7a2de86eeed869647623456
Patricio Ferraggi Ares <pattferraggi@gmail.com>
2022-07-27 18:19:20 -0300

    add local nix-shell

--- chapter5/src/Ch5.purs
+++ chapter5/src/Ch5.purs
@@ -1,9 +1,12 @@
 module Ch5 where
 
 import Data.List (List(..), reverse, (:))
+import Data.Maybe (Maybe(..))
+import Data.Show (class Show)
 import Effect (Effect)
 import Effect.Console (log)
 import Prelude (Unit, discard, show, (+))
+import Prim.RowList (Nil)
 
 flip :: forall a b c. (a -> b -> c) -> b -> a -> c
 flip f x y = f y x
@@ -47,6 +50,35 @@ length :: forall a. List a -> Int
 length Nil    = 0
 length (_:xs) = 1 + length xs
 
+length' :: forall a. List a -> Int
+length' l  = go 0 l where
+  go :: Int -> List a -> Int
+  go acc Nil    = acc
+  go acc (_:xs) = go (acc + 1) xs 
+
+head :: forall  a. List a -> Maybe a
+head Nil    = Nothing
+head (x:_) = Just x
+
+tail :: forall a. List a -> Maybe (List a)
+tail Nil    = Nothing
+tail (_:xs) = Just xs
+
+last :: forall a. List a -> Maybe a
+last Nil = Nothing
+last (_:xs) = head $ reverse xs
+
+last' :: forall a. List a -> Maybe a
+last' l = go Nothing l where
+  go :: Maybe a -> List a -> Maybe a
+  go prev Nil = prev
+  go _ (x:xs) = go (Just x) xs 
+
+last'' :: forall a. List a -> Maybe a
+last'' Nil     = Nothing
+last'' (x:Nil) = Just x
+last'' (_:xs)  = last'' xs 
+
 test :: Effect Unit
 test = do
   log $ show $ flip const 1 2
@@ -57,3 +89,15 @@ test = do
   log $ show $ snoc' (1 : 2 : Nil) 3
   log $ show $ snoc (1 : 2 : Nil) 3
   log $ show $ length (1 : 2 : 3 : Nil)
+  log $ show $ length' (1 : 2 : 3 : Nil)
+  log $ show (head Nil :: Maybe Unit)
+  log $ show $ head ("abc" : "123" : Nil)
+  log $ show $ tail (Nil :: List Unit)
+  log $ show $ tail ("abc" : "123" : Nil)
+  log $ show $ (last Nil :: Maybe Unit)
+  log $ show $ last ("a" : "b" : "c" : Nil)
+  log $ show $ (last' Nil :: Maybe Unit)
+  log $ show $ last' ("a" : "b" : "c" : Nil)
+  log $ show $ (last'' Nil :: Maybe Unit)
+  log $ show $ last'' ("a" : "b" : "c" : Nil)
+
